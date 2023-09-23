c57b2fd58f513d5d2391be55153ed744e7649c91
esalesandrxx <alessrodriguez98@gmail.com>
2023-09-23 12:18:11 -0400

    1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.applifycu"
             artifactId = "bugsend"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
